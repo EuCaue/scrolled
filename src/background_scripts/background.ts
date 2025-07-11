d0c311494d38827404626ad2ecb0389bfa97346b
EuCaue <souzacaue@proton.me>
2025-07-10 23:40:53 -0300

    feat(content-script): run on all pages after installation

--- src/background_scripts/background.ts
+++ src/background_scripts/background.ts
@@ -1,3 +1,26 @@
+browser.runtime.onInstalled.addListener(async () => {
+  try {
+    const tabs = await browser.tabs.query({});
+    for (const tab of tabs) {
+      if (!tab.id) continue;
+
+      try {
+        await browser.scripting.executeScript({
+          target: {
+            tabId: tab.id,
+            allFrames: true,
+          },
+          files: ["../content_scripts/scrollListener.js"],
+        });
+      } catch (err) {
+        console.error(`Failed to execute script in tab ${tab.id}:`, err);
+      }
+    }
+  } catch (err) {
+    console.error("Failed to query tabs:", err);
+  }
+});
+
 browser.runtime.onMessage.addListener((message) => {
   if (message.type === "settings-update") {
     browser.tabs.query({}).then((tabs) => {
@@ -28,6 +51,7 @@ browser.runtime.onConnect.addListener((port) => {
     });
 
     port.onMessage.addListener((msg) => {
+      // @ts-ignore
       if (msg.type === "popup-closed") {
         browser.tabs.query({}).then((tabs) => {
           for (const tab of tabs) {
